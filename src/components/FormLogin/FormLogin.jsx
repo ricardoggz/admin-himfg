5e63ddc81c606ca97683416ae388c702eb78fa76
ricardoggz <ricardo_ggzicm@outlook.com>
2023-06-26 11:13:59 -0600

    [add]: login styles

--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,17 +1,27 @@
-import {Button, Form} from 'react-bootstrap'
+import {Button, Form, Image} from 'react-bootstrap'
 import { formLogin } from './formLogin.module.css'
 
 export const FormLogin = ()=>{
     return (
         <section className={formLogin}>
+        <figure>
+            <Image
+                src="https://res.cloudinary.com/diuxbqmn5/image/upload/v1677114497/himfg-logo_ewzx59.webp"
+                rounded
+                alt='HIMFG LOGO'
+            />
+        </figure>
+        <span className='text-secondary'>
+            Administrativo HIMFG
+        </span>
         <Form border='primary'>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Usuario</Form.Label>
-                <Form.Control type="text"/>
+                <Form.Control type="text" required/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password"/>
+                <Form.Control type="password" required/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Label>Departamento</Form.Label>
