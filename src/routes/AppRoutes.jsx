60a01608401074e02ea985b5dfdb774c5bc31863
ricardoggz <ricardo_ggzicm@outlook.com>
2023-06-26 11:57:44 -0600

    [change]: router dashboard

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,7 @@ export const AppRoutes = ()=>{
     return (
         <Routes>
             <Route path='/' element={<Home />}/>
-            <Route path='/dashboard' element={<Dashboard />}/>
+            <Route path='/dashboard/*' element={<Dashboard />}/>
         </Routes>
     )
 }
