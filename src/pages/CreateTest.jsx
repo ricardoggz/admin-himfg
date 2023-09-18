99fae6f7b1125777bc05f2f83fed6a9158473ab6
ricardoggz <ricardo_ggzicm@outlook.com>
2023-09-18 13:37:01 -0600

    [add]: option dynamic

--- src/pages/CreateTest.jsx
+++ src/pages/CreateTest.jsx
@@ -4,12 +4,22 @@ import { Container, Button, Form } from "react-bootstrap"
 
 export const CreateTest = ()=>{
     const [isOpened, setIsOpened] = useState(false)
+    const [option, setOption] = useState(null)
+    const [options, setOptions] = useState([])
     const showForm = ()=> setIsOpened(!isOpened)
+    const handleChange = (evt)=>setOption({
+        [evt.target.name]: evt.target.value
+    })
+    const handleOptionSubmit = (evt)=>{
+        evt.preventDefault()
+        console.log('submit')
+        return setOptions(options.concat([option]))
+    }
     const params = useParams()
     const id = parseInt(params.id)
     return (
         <Container>
-            <h2 className="text-center pb-5">
+            <h2 className="text-center pb-5">   
                 Crear test
             </h2>
             <div className="d-flex justify-content-center pb-5">
@@ -43,8 +53,36 @@ export const CreateTest = ()=>{
                     required
                     />
                     </Form.Group>
+                    <Form.Label>Opciones:</Form.Label>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Valor:</Form.Label>
+                    <Form.Control
+                    type="text"
+                    required
+                    name='option_name'
+                    onChange={handleChange}
+                    />
+                    <Button variant="info" type='submit' onClick={handleOptionSubmit}>Agregar opción</Button>
+                    </Form.Group>
+                    <div className="mt-5">
+                        {
+                            options.length > 0 ?
+                            <>
+                                {options.map((option, i)=>(
+                                    <div key={i} class="border">
+                                        <span>{option.option_name}</span>
+                                        <br />
+                                        <label>Opción correcta</label>
+                                        <br />
+                                        <input type='radio' name='option_value'/>
+                                    </div>
+                                ))}
+                            </>
+                            :
+                            null
+                        }
+                    </div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Valor de pregunta:</Form.Label>
                     <Form.Control
                     type="number"
                     min="1"
@@ -53,7 +91,7 @@ export const CreateTest = ()=>{
                     required
                     />
                     </Form.Group>
-                    <Button>Agregar Pregunta</Button>
+                    <Button>Guardar Pregunta</Button>
                 </Form>
             }
         </Container>
